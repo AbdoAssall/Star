253efef8d61a73bf49e7f387ec65b62caf0df2a6
Abdo Assal <abdoassal85@gmail.com>
2024-05-06 22:16:47 +0300

    add path to the navbar-brand

--- src/navbar.js
+++ src/navbar.js
@@ -10,7 +10,7 @@ export default class Navbar extends Component {
         return <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-tranparent">
                 <div className="container-fluid">
-                    <a className="navbar-brand fw-bold" href="#">Star</a>
+                    <a className="navbar-brand fw-bold" href="/home">Star</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
