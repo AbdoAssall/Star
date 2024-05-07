048c5a0e48cc75870b967a3854b2d1286ca0a74e
Abdo Assal <abdoassal85@gmail.com>
2024-05-07 19:30:20 +0300

    Delete the unused variable in src/footer.js.

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 export default function Footer(){
     
@@ -6,8 +5,8 @@ export default function Footer(){
         <div className="container-fluid">
         <div className="border-top  border-dark-subtle mt-4 p-4 position-relative">
         <p className="text-center m-0">&copy; 2024 <a className="icon-link link-light" href="https://github.com/AbdoAssall"  target="blank"> Abdo Assal</a> . All rights reserved.</p>
-        {/* <Link className="text-end btn rounded-circle border-light icon-up"><i class="fa-solid fa-arrow-up"></i></Link> */}
         </div>
         </div>
     </div>
 }
+//<Link className="text-end btn rounded-circle border-light icon-up"><i class="fa-solid fa-arrow-up"></i></Link> 
